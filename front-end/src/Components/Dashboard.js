77379ef5e20cd8d6269388ac0070449beadc01ae
Alex Martinez <alexmartinez0071234@gmail.com>
2020-09-17 19:43:44 -0400

    dashboard HTML done

--- front-end/src/Components/Dashboard.js
+++ front-end/src/Components/Dashboard.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import Navbar from '../Components/Navbar';
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../Components/Navbar";
 
-const Dashboard = () =>{
-    return(
-        <div>
-            <h1>This is Dashboard</h1>
-            <Navbar/>
-            <Link to='/editAddress'>Edit Address</Link>
+const Dashboard = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="mainContainer">
+        <div className="leftContainer">
+          <h3>Welcome back, Insert Username Here</h3>
+          <h3>My Account</h3>
         </div>
-        
-    )
+
+        <div className="userInfo">
+          <p>Name</p>
+          <p>Street Address</p>
+          <p>Apt/Suite</p>
+          <p>Zip Code</p>
+          <p>City</p>
+          <Link to="/editAddress">Edit Address</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;

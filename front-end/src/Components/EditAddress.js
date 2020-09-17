dfcc24bf5e9cf1b090b2e7fd2b82f8f25c3677c7
Alex Martinez <alexmartinez0071234@gmail.com>
2020-09-17 19:58:26 -0400

    edit HTML done

--- front-end/src/Components/EditAddress.js
+++ front-end/src/Components/EditAddress.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const EditAddress = () => {
-    return(
-        <div>
-            <p>Edit Address</p>
-            <Link to='/dashboard'>Back to Dashboard</Link>
-        </div>
-    )
+  return (
+    <div>
+      <Navbar />
+      <h2>Edit Your Address</h2>
+      <form>
+        <input placeholder="Country" />
+        <input placeholder="Full Name" />
+        <input placeholder="Street Address" />
+        <input placeholder="Apt/Suite" />
+        <input placeholder="Zip Code" />
+        <input placeholder="City" />
+      </form>
+      <Link to="/dashboard">Back to Dashboard</Link>
+      <button type="submit">Update</button>
+    </div>
+  );
 };
 
 export default EditAddress;

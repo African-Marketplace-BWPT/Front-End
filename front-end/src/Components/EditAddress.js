0ef525dd5ad41ea9cf96df40fe49cd2bd127cac0
Alex Martinez <alexmartinez0071234@gmail.com>
2020-09-18 15:35:29 -0400

    styling progress

--- front-end/src/Components/EditAddress.js
+++ front-end/src/Components/EditAddress.js
@@ -1,22 +1,65 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
+import styled from "styled-components";
+
+const Title = styled.h2`
+  color: #f07736;
+  text-decoration: underline;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  * {
+    margin: 1%;
+  }
+`;
+
+const StyledButton = styled.button`
+  text-decoration: none;
+  color: white;
+  letter-spacing: 0.3rem;
+  padding: 0.4rem 2rem;
+  font-size: 1.2rem;
+  background-color: #f07736;
+  border: 2px solid #f07736;
+  border-radius: 5px;
+  transition: background 0.3s ease-in-out, color 0.3s ease-in-out;
+
+  &:focus {
+    border: 2px solid white;
+  }
+
+  &:hover {
+    background-color: white;
+    color: #f07736;
+  }
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
 
 const EditAddress = () => {
   return (
     <div>
       <Navbar />
-      <h2>Edit Your Address</h2>
-      <form>
+      <StyledForm>
+        <Title>Edit Your Address</Title>
         <input placeholder="Country" />
         <input placeholder="Full Name" />
         <input placeholder="Street Address" />
         <input placeholder="Apt/Suite" />
         <input placeholder="Zip Code" />
         <input placeholder="City" />
-      </form>
-      <Link to="/dashboard">Back to Dashboard</Link>
-      <button type="submit">Update</button>
+      </StyledForm>
+      <Bottom>
+        <a href="/dashboard">Back to Dashboard</a>
+        <StyledButton type="submit">UPDATE</StyledButton>
+      </Bottom>
     </div>
   );
 };
